72afde15d631cad81bfe7918405b211d209e4f0c
Marcos Fernando <marcos.fersh@gmail.com>
2023-07-09 23:35:23 -0300

    Finalizacao do Ver Contas

--- contas/views.py
+++ contas/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from perfil.models import Categoria
-from .models import ContaPagar
+from .models import ContaPaga, ContaPagar
 from django.contrib import messages
 from django.contrib.messages import constants
+from datetime import datetime
 
 def definir_contas(request):
     if request.method == "GET":
@@ -27,3 +28,19 @@ def definir_contas(request):
 
         messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
         return redirect('/contas/definir_contas')
+    
+def ver_contas(request):
+    MES_ATUAL = datetime.now().month
+    DIA_ATUAL = datetime.now().day
+    
+    contas = ContaPagar.objects.all()
+
+    contas_pagas = ContaPaga.objects.filter(data_pagamento__month=MES_ATUAL).values('conta')
+
+    contas_vencidas = contas.filter(dia_pagamento__lt=DIA_ATUAL).exclude(id__in=contas_pagas)
+    
+    contas_proximas_vencimento = contas.filter(dia_pagamento__lte = DIA_ATUAL + 5).filter(dia_pagamento__gte=DIA_ATUAL).exclude(id__in=contas_pagas)
+    
+    restantes = contas.exclude(id__in=contas_vencidas).exclude(id__in=contas_pagas).exclude(id__in=contas_proximas_vencimento)
+
+    return render(request, 'ver_contas.html', {'contas_vencidas': contas_vencidas, 'contas_proximas_vencimento': contas_proximas_vencimento, 'restantes': restantes})    
